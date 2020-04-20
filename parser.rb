da5fde2fb68007292979d9d313f6ca009b0fb0c2
Neeraj Negi <neeraj.negi@loconav.com>
2020-04-20 10:00:39 +0530

    Fix and finish the scrapper, add gitignore

--- parser.rb
+++ parser.rb
@@ -1,29 +1,54 @@
 ## Takes sarathi html file as input
 require 'nokogiri'
 
-file_name = 'sarathi.html'
+file_name = 'sarathi.htm'
 file = File.open(file_name)
 page = Nokogiri::HTML(file.read)
 
 license_number = page.css('thead')[0].css('span.text').text
-status = page.css('tbody')[1].css('td')[1].text
+status = page.css('table')[2].css('td')[1].text
 
-date_of_issue = page.css('table')[2].css('td')[1].text
-date_of_last_transaction = page.css('table')[2].css('td')[3].text
-last_transacted_at = page.css('table')[3].css('td')[1].text
+date_of_issue = page.css('table')[3].css('td')[1].text
+date_of_last_transaction = page.css('table')[3].css('td')[3].text
+last_transacted_at = page.css('table')[4].css('td')[1].text
 
 ## DL validity
-nt_dl_vailidity_starts_at = page.css('table')[4].css('td')[3].text
-nt_dl_vailidity_ends_at = page.css('table')[4].css('td')[5].text
-t_dl_vailidity_starts_at = page.css('table')[4].css('td')[8].text
-t_dl_vailidity_ends_at = page.css('table')[4].css('td')[10].text
-hazardous_valid_till = page.css('table')[4].css('td')[12].text
-hill_valid_till = page.css('table')[4].css('td')[14].text
+nt_dl_vailidity_starts_at = page.css('table')[5].css('td')[3].text
+nt_dl_vailidity_ends_at = page.css('table')[5].css('td')[5].text
+t_dl_vailidity_starts_at = page.css('table')[5].css('td')[8].text
+t_dl_vailidity_ends_at = page.css('table')[5].css('td')[10].text
+hazardous_valid_till = page.css('table')[5].css('td')[12].text
+hill_valid_till = page.css('table')[5].css('td')[14].text
 
-##COV Details
-cov_details = Hash.new([])
-page.css('table')[6].css('tr')[1].css('tr')[1..-1].each do |detail|
-  cov_details << [detail.css('td')[0], detail.css('td')[1], detail.css('td')[2]]
+## COV Details
+cov_details = []
+page.css('table')[7].css('table')[0].css('tr')[1..-1].each do |detail|
+  cov_details <<  { category: detail.css('td')[0].text,
+                    class: detail.css('td')[1].text.gsub(' ', ''),
+                    issue_date: detail.css('td')[2].text
+                  }
 end
+
+## Badge Details
+badge_details = []
+page.css('table')[9].css('table')[0].css('tr')[1..-1].each do |detail|
+  badge_details <<  { category: detail.css('td')[0].text,
+                    class: detail.css('td')[1].text.gsub(' ', ''),
+                    issue_date: detail.css('td')[2].text
+                  }
+end
+
+puts license_number
+puts status
+puts date_of_issue
+puts date_of_last_transaction
+puts last_transacted_at
+puts nt_dl_vailidity_starts_at
+puts nt_dl_vailidity_ends_at
+puts t_dl_vailidity_starts_at
+puts t_dl_vailidity_ends_at
+puts hazardous_valid_till
+puts hill_valid_till
 puts cov_details
+puts badge_details
 
